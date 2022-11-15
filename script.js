7dce475b5c04dc6fe8ab41d5a51f3d9cd2d93274
Parviz13 <112949085+Parviz13@users.noreply.github.com>
2022-11-15 15:12:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+let inpNeeds = document.querySelectorAll('.myBlue input')
+let allInps = document.querySelectorAll('form input')
+let btn = document.querySelector('.submit')
+let form = document.forms.reg
+
+let pattern = {
+    name: /^[a-z ,.'-]+$/i,
+    age: /^\S[0-9]{0,3}$/g,
+    email: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+    aboutYou: /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/, 
+    html: /^[a-z ,.'-]+$/,
+    css:    /^[a-z ,.'-]+$/,
+    surname: /^[a-z ,.'-]+$/i,
+    password: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\S+$).{8,}$/,
+    phone: /^998[012345789][0-9]{8}$/g,
+
+}
+
+
+function validate(regex, field) {
+    if(regex.test(field.value)) {
+        field.parentElement.classList.remove('invalid')
+        field.parentElement.classList.add('valid')
+    } else {
+        field.parentElement.classList.remove('valid')
+        field.parentElement.classList.add('invalid')
+    }
+}
+
+allInps.forEach(inp => {
+    inp.onkeyup = () => {
+
+        validate(pattern[inp.name], inp)
+    }
+})
+
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+    let countErrs = 0
+
+    inpNeeds.forEach(inp => {
+        if(inp.value.length < 1 || inp.parentElement.classList.contains('invalid')) {
+            inp.parentElement.classList.add('invalid')
+            countErrs++
+        } else {
+            inp.parentElement.classList.remove('invalid')
+        }
+    });
+console.log(countErrs);
+
+    if(countErrs > 0) {
+        console.log('Error')
+    } else {
+        submit(form)
+    }
+}
+
+
+function submit(formElement) {
+    let user = {}
+
+    let fm = new FormData(formElement)
+
+    fm.forEach((value, key) => {
+        user[key] = value
+    })
+
+    console.log(user);
+}
+
+// const loaderContainer = document.querySelector('.loader-container');
+
+
+btn.onclick = () => {
+btn.style.backgroundColor = "black"
+//   setTimeout ()
+// window.addEventListener('load', () => {
+//   // ...
+// });
+// window.addEventListener('load', () => {
+//   loaderContainer.style.display = 'none';
+//   window.addEventListener('load', () => {
+//     loaderContainer.classList.add('loader-container-hidden');
+// });
+// });
+}
+
+
+
+
+
+
+
+
